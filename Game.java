d427da78a2776f83da6caaed8ced9bce559c89d3
TheLittleYH <50745071+TheLittleYH@users.noreply.github.com>
2019-05-18 20:44:27 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -174,15 +174,4 @@ public class Game{
         comb[i] = 1;
         return true;
     }
-    private boolean arrayEquals(int[] arr1, int[] arr2){
-        if(arr1.length != arr2.length){
-            return false;
-        }
-        for(int i = 0; i < arr1.length; i++){
-            if(arr1[i] != arr2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
 }
